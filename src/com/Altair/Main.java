a77b922158dc7efed73ee7060e3068e71220034b
Manan1612 <mananpatel161200@gmail.com>
2025-04-11 09:17:21 -0400

    Added New File

---
+++ src/com/Altair/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package com.Altair;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello World!");
+    }
+}
